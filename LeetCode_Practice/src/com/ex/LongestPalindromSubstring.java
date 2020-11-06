9f895591a7c88e706011c2c2e78ead94e17a2b60
manda <manda@Sayantan.mshome.net>
2020-11-07 00:11:15 +0530

    improved code

--- LeetCode_Practice/src/com/ex/LongestPalindromSubstring.java
+++ LeetCode_Practice/src/com/ex/LongestPalindromSubstring.java
@@ -2,40 +2,55 @@ package com.ex;
 
 public class LongestPalindromSubstring {
 	
-	public static String getResult(String str){
-		if(str.length()==1)
-			return str;
-		String res="";
-		for(int i=0;i<str.length();i++) {
-			for(int j=i;j<str.length();j++) {
-				if(str.charAt(i)==str.charAt(j)) {
-					int s = i;
-					int e = j;
-					while(s<=e) {
-						if(str.charAt(s)!=str.charAt(e)) {
-							break;
-						}
-						s++;
-						e--;
-						if(s>=e) {
-							if(res.length()<(j+1-i)) {
-								res = str.substring(i,j+1);
-								if(j==(str.length()-1))
-									return res;
-							}
-						}
-					}
-				} 
-				
-			}
-		}
-		return res;
-		
+	public static String getResult(String s){
+		if(s == null || s.length() < 1) 
+            return "";
+        int start = 0, end = 0;
+        int maxLen = 1;
+        int n = s.length();
+        
+        // odd Length
+        for(int i=0;i < n-1 ;i++){
+            int l = i;
+            int r = i;
+            while(l >= 0 && r < n){
+                if(s.charAt(l) == s.charAt(r)){
+                    l--;
+                    r++;
+                }else
+                    break;
+            }
+            int len = r - l - 1;
+            if(len > maxLen){
+                maxLen = len;
+                start = l+1;
+                end = r-1;
+            }
+        }
+        // Even Length
+        for(int i=0;i < n-1 ;i++){
+            int l = i;
+            int r = i+1;
+            while(l >= 0 && r < n){
+                if(s.charAt(l) == s.charAt(r)){
+                    l--;
+                    r++;
+                }else
+                    break;
+            }
+            int len = r - l - 1;
+            if(len > maxLen){
+                maxLen = len;
+                start = l+1;
+                end = r-1;
+            }
+        }
+        return s.substring(start,end+1);  
 	}
 
 	public static void main(String[] args) {
 
-		System.out.println(getResult("cbbd"));
+		System.out.println(getResult("cbabd"));
 		
 	}
 
