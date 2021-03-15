81f1490573b0f4cdce01d97f5acb3867f21d9ce2
manda <manda@Sayantan>
2021-03-16 02:45:23 +0530

    deadlock

--- LeetCode_Practice/src/com/ex/DeadLock_Demo.java
+++ LeetCode_Practice/src/com/ex/DeadLock_Demo.java
@@ -3,8 +3,24 @@ package com.ex;
 public class DeadLock_Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+			sharedResource s1 = new sharedResource();
+			sharedResource s2 = new sharedResource();
+			
+		
+			Runnable t1 = () -> {   // lambda expression
+	         System.out.println("Runnable with Lambda Expression t1");
+	         s1.test1(s2);
+			};
+	      
+			Runnable t2 = () -> {   // lambda expression
+		         System.out.println("Runnable with Lambda Expression t2");
+		         s2.test1(s1);
+		    };
+		
+		    new Thread(t1).start();
+		    new Thread(t2).start();
+		
 	}
 
 }
---
+++ LeetCode_Practice/src/com/ex/sharedResource.java
@@ -0,0 +1,27 @@
+package com.ex;
+
+class sharedResource{
+	
+	synchronized void test1(sharedResource s) {
+		
+		System.out.println("test1-begin"+Thread.currentThread().getName());
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		s.test2();
+	}
+	
+	synchronized void test2() {
+		System.out.println("test2-begin"+Thread.currentThread().getName());
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
