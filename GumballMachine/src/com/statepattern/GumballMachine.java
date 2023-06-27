564ea98872cc25418a2ec9d9622094c2b81c0d55
Sayantan <mandalsayantan95@gmail.com>
2023-06-28 01:22:16 +0530

    State Pattern - GumballMachine - completed

--- GumballMachine/src/com/statepattern/GumballMachine.java
+++ GumballMachine/src/com/statepattern/GumballMachine.java
@@ -12,6 +12,17 @@ public class GumballMachine {
 	
 	State state = soldOutState;
 	int count = 0;
+
+	public GumballMachine(int numberGumballs) {
+		this.soldOutState = new SoldOutState(this);
+		this.noQuarterState =new NoQuarterState(this);
+		this.hasQuarterState = new HasQuarterState(this);
+		this.soldState = new SoldState(this);
+		this.count = numberGumballs;
+		if(numberGumballs > 0){
+			state = noQuarterState;
+		}
+	}
 	
 	public void insertQuarter() {
 		 state.insertQuarter();
